57d16e42de21f4e70f94079bfa6f2f598172fc2b
Jordan <jordan.d.carlton@gmail.com>
2021-10-12 19:06:01 +0100

    Fix navbar not displaying on window reload

--- MyFace/wwwroot/js/site.js
+++ MyFace/wwwroot/js/site.js
@@ -1,8 +1,10 @@
 /* Toggle between showing and hiding the navigation menu
 links when the user clicks on the hamburger menu / bar icon */
 
+var max_width = 700;
+
 function toggleBurgerMenu(){
-    var widthMediaSmall = window.matchMedia("(max-width: 900px)")
+    var widthMediaSmall = window.matchMedia(`(max-width: ${max_width}px )`)
     var x = document.getElementById("myLinks");
     if (widthMediaSmall){
         if (x.style.display === "block") {
@@ -16,11 +18,11 @@ function toggleBurgerMenu(){
 function turnOnOrOffBurgerMenu(){
     var x = document.getElementById("myLinks");
     
-    if(window.innerWidth > 900){
+    if(window.innerWidth > max_width){
         if (x.style.display === "block" || x.style.display === "none"){
             x.style.display = "flex";
         }
-    } else if (window.innerWidth < 900){
+    } else if (window.innerWidth < max_width){
         if (x.style.display === "flex"){
             x.style.display = "none";
         }
@@ -30,5 +32,6 @@ function turnOnOrOffBurgerMenu(){
 var x = document.getElementById("myLinks");
 x.style.display = "none";
 window.onresize = turnOnOrOffBurgerMenu;
+turnOnOrOffBurgerMenu()
 
 
