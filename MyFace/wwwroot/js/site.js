d6cbfbaac5b4915af63ef2960f20c065bca3a008
Jordan <jordan.d.carlton@gmail.com>
2021-10-12 17:37:05 +0100

    Add navbar burger menu media query

--- MyFace/wwwroot/js/site.js
+++ MyFace/wwwroot/js/site.js
@@ -1,11 +1,16 @@
 /* Toggle between showing and hiding the navigation menu
 links when the user clicks on the hamburger menu / bar icon */
 
-function myFunction() {
+var widthMedia = window.matchMedia("(max-width:900px)")
+widthMedia.addEventListener(myFunction)
+
+function myFunction(widthMedia) {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block") {
+    if (x.style.display === "block" && widthMedia === true) {
         x.style.display = "none";
     } else {
         x.style.display = "block";
     }
 }
+
+
