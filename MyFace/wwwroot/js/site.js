e3de53a43b2a8686213f3f3f1b6c4fefdfb3fd2b
Jordan <jordan.d.carlton@gmail.com>
2021-10-12 17:55:54 +0100

    Add media query for navbar burger menu

--- MyFace/wwwroot/js/site.js
+++ MyFace/wwwroot/js/site.js
@@ -1,16 +1,18 @@
 /* Toggle between showing and hiding the navigation menu
 links when the user clicks on the hamburger menu / bar icon */
 
-var widthMedia = window.matchMedia("(max-width:900px)")
-widthMedia.addEventListener(myFunction)
-
-function myFunction(widthMedia) {
+function test(widthMedia) {
     var x = document.getElementById("myLinks");
-    if (x.style.display === "block" && widthMedia === true) {
+    if (x.style.display === "block" && widthMedia) {
         x.style.display = "none";
-    } else {
+    } else if (widthMedia){
         x.style.display = "block";
     }
+}
+
+function myFunction(){
+    var widthMedia = window.matchMedia("(max-width: 900px)")
+    test(widthMedia)
 }
 
 
